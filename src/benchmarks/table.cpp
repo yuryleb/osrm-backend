452efe5cbe2344ccbfb4aa42d76f28c958e0f152
Daniel Patterson <daniel@mapbox.com>
2017-02-16 05:46:38 -0800

    Add benchmarking tool for the table plugin

---
+++ src/benchmarks/table.cpp
@@ -0,0 +1,96 @@
+#include "util/timing_util.hpp"
+
+#include "osrm/table_parameters.hpp"
+
+#include "osrm/coordinate.hpp"
+#include "osrm/engine_config.hpp"
+#include "osrm/json_container.hpp"
+#include "storage/io.hpp"
+#include "storage/serialization.hpp"
+
+#include "osrm/osrm.hpp"
+#include "osrm/status.hpp"
+
+#include <boost/assert.hpp>
+
+#include <exception>
+#include <iostream>
+#include <string>
+#include <utility>
+
+#include <cstdlib>
+
+int main(int argc, const char *argv[]) try
+{
+    if (argc < 2)
+    {
+        std::cerr << "Usage: " << argv[0] << " data.osrm\n";
+        return EXIT_FAILURE;
+    }
+
+    using namespace osrm;
+
+    std::vector<util::Coordinate> coordinates;
+    // Loading list of coordinates
+    {
+        using namespace osrm::storage;
+        io::FileReader nodes_file(std::string(argv[1]) + ".nodes",
+                                  io::FileReader::HasNoFingerprint);
+        std::size_t count = 0;
+        std::vector<OSMNodeID> nodes;
+
+        nodes_file.ReadInto(count);
+        coordinates.resize(count);
+        nodes.reserve(count);
+
+        serialization::readNodes(nodes_file, coordinates.data(), nodes, count);
+    }
+
+    // Configure based on a .osrm base path, and no datasets in shared mem from osrm-datastore
+    EngineConfig config;
+    config.storage_config = {argv[1]};
+    config.use_shared_memory = false;
+
+    // Routing machine with several services (such as Route, Table, Nearest, Trip, Match)
+    OSRM osrm{config};
+
+    // Shuffle the coordinate list randomly, then we'll simply use the first N coordinates
+    // to build our random table queries
+    std::random_shuffle(coordinates.begin(), coordinates.end());
+
+    auto runtest = [&](const int coordcount) -> void {
+        TableParameters params;
+        for (int i = 0; i < coordcount; i++)
+        {
+            params.coordinates.push_back(coordinates[i]);
+        }
+
+        TIMER_START(routes);
+        auto NUM = 10;
+        for (int i = 0; i < NUM; ++i)
+        {
+            json::Object result;
+            const auto rc = osrm.Table(params, result);
+            if (rc != Status::Ok)
+            {
+                return;
+            }
+        }
+        TIMER_STOP(routes);
+        std::cout << params.coordinates.size() << "," << (TIMER_MSEC(routes) / 1000. / NUM)
+                  << std::endl;
+    };
+
+    std::cout << "Coordinates,Duration in seconds" << std::endl;
+    for (int i = 0; i < 3000; i += 10)
+    {
+        runtest(i);
+    }
+
+    return EXIT_SUCCESS;
+}
+catch (const std::exception &e)
+{
+    std::cerr << "Error: " << e.what() << std::endl;
+    return EXIT_FAILURE;
+}
